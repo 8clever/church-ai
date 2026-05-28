a214bee4f73d82845646c01ddd051f19d8ede3b5
root <godofluck89@gmail.com>
2026-05-28 17:14:56 +0000

    fix

--- app/ai.ts
+++ app/ai.ts
@@ -34,6 +34,12 @@ export async function loadLLM(props: {
     onDownload? (value: number): void
 } = {}): Promise<Session> {
     if (!('LanguageModel' in window)) {
+        const config = {
+            apiKey: 'dummy', // Required for now by the loader
+            modelName: 'Llama-3.2-1B-Instruct-q4f16_1-MLC'
+        };
+        // @ts-ignore
+        window.WEBLLM_CONFIG = config;
         // @ts-ignore
         await import('prompt-api-polyfill');
     }
